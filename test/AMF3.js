69d6ea1f699466686af87f8ce623eff389247e62
Zaseth <hahano>
2019-09-13 19:20:14 +0200

    Add test for AMF3

---
+++ test/AMF3.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const it = require('tape')
+const ByteArray = require('../src/')
+
+it('Can write/read AMF3 values representing their marker', (tape) => {
+  const ba = new ByteArray()
+
+  ba.writeObject(null)
+  ba.writeObject(undefined)
+  ba.writeObject(true)
+  ba.writeObject(false)
+
+  ba.position = 0
+
+  tape.equal(ba.readObject(), null)
+  tape.equal(ba.readObject(), undefined)
+  tape.ok(ba.readObject())
+  tape.notOk(ba.readObject())
+
+  tape.end()
+})
+
+it('Can write/read AMF3 numbers', (tape) => {
+  const ba = new ByteArray()
+
+  ba.writeObject(100)
+  ba.writeObject(-5)
+  ba.writeObject(563654)
+  ba.writeObject(1.23)
+  ba.writeObject(268435455)
+  ba.writeObject(-268435456)
+  ba.writeObject(268435456)
+
+  ba.position = 0
+
+  tape.equal(ba.readObject(), 100)
+  tape.equal(ba.readObject(), -5)
+  tape.equal(ba.readObject(), 563654)
+  tape.equal(ba.readObject(), 1.23)
+  tape.equal(ba.readObject(), 268435455)
+  tape.equal(ba.readObject(), -268435456)
+  tape.equal(ba.readByte(), 5)
+  ba.position--
+  tape.equal(ba.readObject(), 268435456)
+
+  tape.end()
+})
