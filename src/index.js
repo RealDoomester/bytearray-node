bb624eaaaf3e4aadb16f2b63a3b28a3970a1cc81
Zaseth <hahano>
2020-01-28 13:57:16 +0100

    Use Buffer.byteLength, not value.length. This fixes altcodes

--- src/index.js
+++ src/index.js
@@ -287,7 +287,17 @@ module.exports = class ByteArray {
     this.position += length
 
     if (encodingExists(charset)) {
-      return decode(this.buffer.slice(position, this.position), charset)
+      const b = this.buffer.slice(position, this.position)
+      const stripBOM = (charset === 'utf8' || charset === 'utf-8') && b.length >= 3 && b[0] === 0xEF && b[1] === 0xBB && b[2] === 0xBF
+      const value = decode(b, charset, { stripBOM })
+
+      stripBOM ? length -= 3 : 0
+
+      if (Buffer.byteLength(value) !== length) {
+        throw new RangeError('End of buffer was encountered.')
+      }
+
+      return value
     } else {
       throw new Error(`Invalid character set: '${charset}'.`)
     }
